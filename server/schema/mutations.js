0eb7275063f2c30ef66b53a84186d96485283127
Alexander Hogle <alexanderhogle@gmail.com>
2022-01-18 19:33:14 -0600

    adds signup, logout, and login mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,23 +1,41 @@
-const { GraphQLObjectType } = require("graphql");
-const graphql = require("graphql");
+const { GraphQLObjectType } = require('graphql');
+const graphql = require('graphql');
 const { GraphGLObjectType, GraphQLString } = graphql;
-const UserType = require("./types/user_type");
-const AuthService = require("../services/auth");
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType({
-  name: "Mutation",
+  name: 'Mutation',
   fields: {
     signup: {
       type: UserType,
       args: {
         email: { type: GraphQLString },
-        password: { type: GraphQLString },
+        password: { type: GraphQLString }
       },
       resolve(parentValue, { email, password }, req) {
         return AuthService.signup({ email, password, req });
-      },
+      }
     },
-  },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
+    }
+  }
 });
 
 module.exports = mutation;
