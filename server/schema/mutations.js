2fae28efe90a37f0693d8d73ad4e955c7e65076b
Alexander Hogle <alexanderhogle@gmail.com>
2022-01-18 18:52:49 -0600

    initial commit

---
+++ server/schema/mutations.js
@@ -0,0 +1,23 @@
+const { GraphQLObjectType } = require("graphql");
+const graphql = require("graphql");
+const { GraphGLObjectType, GraphQLString } = graphql;
+const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req });
+      },
+    },
+  },
+});
+
+module.exports = mutation;
